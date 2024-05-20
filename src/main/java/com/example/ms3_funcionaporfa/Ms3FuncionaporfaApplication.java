bffb58572c929646259f95bda18f997f133c8b80
RODRIGO <rodrigo.meza@utec.edu.pe>
2024-05-19 20:20:57 -0500

    Push

---
+++ src/main/java/com/example/ms3_funcionaporfa/Ms3FuncionaporfaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms3_funcionaporfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms3FuncionaporfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms3FuncionaporfaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ms3_funcionaporfa/Ms3FuncionaporfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms3_funcionaporfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms3FuncionaporfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
